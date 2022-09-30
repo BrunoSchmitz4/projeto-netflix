2d3ff2d5c297f6f06022cd895f817243593e714a
BrunoSchmitz4 <brunosch250@gmail.com>
2022-09-30 16:01:49 -0300

    LinkLists Iniciado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  links_list1: any = [];
   constructor() { }
 
   ngOnInit(): void {
+    // this.links_list1 = [
+    //   {navs: '..freq-ques.component.html', name:'Perguntas frequentes'},
+    //   {navs: '/investors', name: 'Investidores'},
+    //   {navs: '/devices', name: 'Formas de Assistir'},
+    //   {navs: '/info-corp', name: 'Informações Corporativas'},
+    //   {navs: '/original', name: 'Só na Netflix'}
+    // ];
   }
 
 }
